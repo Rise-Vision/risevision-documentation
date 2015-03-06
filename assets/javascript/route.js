2c8fec6f157f12fc9b00360043180f4bb52d590b
rodrigopavezi <rodrigopavezi@gmail.com>
2015-03-05 23:39:27 -0300

    Trying to fix relatives urls

--- assets/javascript/route.js
+++ assets/javascript/route.js
@@ -12,42 +12,42 @@ angular.module("risevision.documentation")
 
         $stateProvider
             .state('developer', {
-                url: '/developer',
+                url: 'developer',
                 templateUrl: 'developer/developer.html',
                 controller: 'DocumentationController'
             })
             .state('developer-developer', {
-                url: '/developer/developer',
+                url: 'developer/developer',
                 templateUrl: 'developer/developer.html',
                 controller: 'DocumentationController'
             })
             .state('developer-post', {
-                url: '/developer/:category/:post',
+                url: 'developer/:category/:post',
                 templateUrl: function(params){ return "{{ site.baseurl }}developer/" + params.category +"/" + params.post + ".html"; },
                 controller: 'DocumentationController'
             })
             .state('developer-post-sub', {
-                url: '/developer/:category/:subCategory/:post',
+                url: 'developer/:category/:subCategory/:post',
                 templateUrl: function(params){ return "{{ site.baseurl }}developer/" + params.category + "/" + params.subCategory + "/" + params.post + ".html"; },
                 controller: 'DocumentationController'
             })
             .state('user', {
-                url: '/user',
+                url: 'user',
                 templateUrl: 'user/user.html',
                 controller: 'DocumentationController'
             })
             .state('user-user', {
-                url: '/user/user',
+                url: 'user/user',
                 templateUrl: 'user/user.html',
                 controller: 'DocumentationController'
             })
             .state('user-post', {
-                url: '/user/:category/:post',
+                url: 'user/:category/:post',
                 templateUrl: function(params){ return "{{ site.baseurl }}user/" + params.category + "/" + params.post + ".html"; },
                 controller: 'DocumentationController'
             })
             .state('user-post-sub', {
-                url: '/user/:category/:subCategory/:post',
+                url: 'user/:category/:subCategory/:post',
                 templateUrl: function(params){ return "{{ site.baseurl }}user/" + params.category + "/" + params.subCategory + "/" + params.post + ".html"; },
                 controller: 'DocumentationController'
             })
