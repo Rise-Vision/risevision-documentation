eb121f98db1684fc936aa4e2bfdf359d8fa5279a
RV-Adrian <adrian.dalimonte@risevision.com>
2015-12-24 15:46:42 -0500

    Simplified event parameters

--- assets/javascript/controllers/ctr-documentation.js
+++ assets/javascript/controllers/ctr-documentation.js
@@ -1,8 +1,4 @@
 angular.module("risevision.documentation")
-    .controller("DocumentationController", function( $scope, $location, documentationTracker ) {
+    .controller("DocumentationController", function($scope,$location,documentationTracker){
     	$scope.documentationTracker = documentationTracker;
-
-    	$scope.promoBannerClicked = function() {
-    		documentationTracker.promoBannerClicked("Rise Display Bundle", "Documentation: Create A Display", true, userState.inRVAFrame());
-    	 };
-    });
+    })
--- assets/javascript/services/svc-documentation-tracker.js
+++ assets/javascript/services/svc-documentation-tracker.js
@@ -1,22 +1,20 @@
 'use strict';
 
-angular.module('risevision.documentation')
+angular.module('risevision.documentation', [
+   "risevision.common.components.analytics"
+])
   .factory('documentationTracker', ['userState', 'segmentAnalytics',
     function (userState, segmentAnalytics) {
-
-      return function (eventName) {
+      return function (eventName, productName, source) {
         if (eventName) {
-          segmentAnalytics.track(eventName);
+          segmentAnalytics.track(eventName, {
+            "Product Name": productName,
+            "Source": source,
+            "user Signed In": userIsSignedIn,
+            "inApp": userState.inRVAFrame(),
+            "companyId": userState.getSelectedCompanyId()
+          });
         }
       };
-
-      this.promoBannerClicked = function (productName, source, userIsSignedIn, inRVAFrame)
-         segmentAnalytics.track("Promotional Banner Clicked", {
-          "Product Name": productName,
-          "Source": source,
-          "user Signed In": userIsSignedIn,
-          "inApp": inRVAFrame
-        });
-
-      }
+    }
   ]);
