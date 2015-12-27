ad04925b334348534dd133110cf85a37bb7a9503
RV-Adrian <adrian.dalimonte@risevision.com>
2015-12-27 04:56:20 -0500

    Stash tracker changed for now

--- assets/javascript/services/svc-documentation-tracker.js
+++ assets/javascript/services/svc-documentation-tracker.js
@@ -1,12 +1,18 @@
 'use strict';
 
-angular.module('risevision.documentation')
+angular.module('risevision.documentation', [
+   "risevision.common.components.analytics"
+])
   .factory('documentationTracker', ['userState', 'segmentAnalytics',
     function (userState, segmentAnalytics) {
-      return function (eventName) {
+      return function (eventName, productName, source) {
         if (eventName) {
           segmentAnalytics.track(eventName, {
-            companyId: userState.getSelectedCompanyId()
+            "Product Name": productName,
+            "Source": source,
+            "user Signed In": userIsSignedIn,
+            "inApp": userState.inRVAFrame(),
+            "companyId": userState.getSelectedCompanyId()
           });
         }
       };
