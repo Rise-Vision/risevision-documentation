f763d65c375c9d8f34424d9692e161cdfa427bf9
Alex Deaconu <alex.deaconu@gmail.com>
2017-05-23 17:09:35 -0400

    Update CH, CS, remove menu options
    Set navSelected for CH based on current page

--- assets/scripts/controllers/ctr-header-navbar.js
+++ assets/scripts/controllers/ctr-header-navbar.js
@@ -1,25 +1,46 @@
 angular.module("risevision.developer")
-  .controller("CommonHeaderNavbarCtrl", ["$rootScope", "$state",
-  function($rootScope, $state) {
+  .controller("CommonHeaderNavbarCtrl", ["$scope", "$rootScope", "$state",
+  function($scope, $rootScope, $state) {
     $rootScope.navOptions = [{
       title: "App Registration",
-      link: $state.href("apps.list")
+      link: $state.href("apps.list"),
+      cid: true,
+      states: [
+        "apps",
+        "apps.withoutPermission",
+        "apps.list",
+        "apps.add",
+        "apps.edit"
+      ]
     },
     {
       title: "API Explorer",
-      link: $state.href("api-explorer.list")
+      link: $state.href("api-explorer.list"),
+      cid: true,
+      states: [
+        "api-explorer",
+        "api-explorer.list",
+        "api-explorer.explore",
+        "api-explorer.explore.endpoint"
+      ]
     },
     {
       title: "Developer Documentation",
-      link: $state.href("documentation", {}, {absolute: true})
-    },
-    {
-      title: "User Documentation",
-      link: "https://risevision.zendesk.com/hc"
-    },
-    {
-      title: "Training",
-      link: "https://store.risevision.com/product/30/rise-training",
-      target: "_blank"
+      link: $state.href("documentation", {}, {absolute: true}),
+      cid: true,
+      states: [
+        "documentation",
+        "documentation-documentation",
+        "documentation-post",
+        "documentation-post-category",
+        "documentation-post-sub",
+        "page-not-found"
+      ]
     }];
+    
+    $scope.navSelected = 'documentation';
+    $rootScope.$on('$stateChangeSuccess', function () {
+      $scope.navSelected = $state.current.name;
+    });
+
   }]);
