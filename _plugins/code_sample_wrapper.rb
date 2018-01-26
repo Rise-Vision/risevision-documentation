cc5cf64b0f1b21263743bdf522b2c211d792d0a0
Francisco Vallarino <fjvallarino@gmail.com>
2018-01-26 16:25:14 -0300

    Upgrade Jekyll to 3.x

--- _plugins/code_sample_wrapper.rb
+++ _plugins/code_sample_wrapper.rb
@@ -8,7 +8,7 @@ module Jekyll
 
       def render(context)
         site = context.registers[:site]
-        converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
+        converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
         output = converter.convert(super(context))
         "<div class=\"code-sample-wrapper\">#{output}</div>"
       end
